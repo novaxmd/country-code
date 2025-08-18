4a84d57da6e17f61124e4aa3646f87a525c59e00
𝗡𝗢𝗩𝗔-𝗫𝗠𝗗 <hanchobmb@gmail.com>
2025-08-18 12:11:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-// Path sahihi ikiwa countries.json ipo ndani ya folder ya api
+// nova xmd countries
 const countries = require("./api/countries.json");
 
 app.get("/", (req, res) => {
